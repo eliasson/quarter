d42935caf8de5563dac980aec1fe52db9d5e1922
Markus Eliasson <markus.eliasson@gmail.com>
2022-09-24 09:46:38 +0200

    Add datetime column for timesheet and timeslot

---
+++ src/Quarter.Core/Migrations/AddTimestampColumns.cs
@@ -0,0 +1,31 @@
+using FluentMigrator;
+
+namespace Quarter.Core.Migrations;
+
+[Migration(0002)]
+public class AddTimestampColumns : Migration
+{
+    public override void Up()
+    {
+        // When Quarter started as a PoC where I set the date column to a string due to pure laziness. Then I forgot
+        // to change it. So add another column with the proper type and gradually remove the use of date column. Then
+        // it can be renamed to just date.
+        Alter.Table("timesheet")
+            .AddColumn("date_ts")
+            .AsDateTime()
+            .Indexed()
+            .Nullable();
+
+        Alter.Table("timeslot")
+            .AddColumn("date_ts")
+            .AsDateTime()
+            .Indexed()
+            .Nullable();
+    }
+
+    public override void Down()
+    {
+        Delete.Column("date_ts").FromTable("timesheet");
+        Delete.Column("date_ts").FromTable("timeslot");
+    }
+}
---
+++ src/Quarter.Core/Migrations/AssignTimestamps.cs
@@ -0,0 +1,18 @@
+using FluentMigrator;
+
+namespace Quarter.Core.Migrations;
+
+[Migration(0003)]
+public class AssignTimestamps : Migration
+{
+    public override void Up()
+    {
+        Execute.Sql("UPDATE timesheet SET date_ts = TO_DATE(date, 'YYYY-MM-DD') WHERE date is not null;");
+        Execute.Sql("UPDATE timeslot SET date_ts = TO_DATE(date, 'YYYY-MM-DD') WHERE date is not null;");
+    }
+
+    public override void Down()
+    {
+        // NOP
+    }
+}
