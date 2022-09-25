3dee71b19f6ac7986913e4f49112e7fec7d32ba7
Markus Eliasson <markus.eliasson@gmail.com>
2022-09-25 15:31:33 +0200

    Render monthly report

--- test/unit/Quarter.UnitTest/Pages/Application/Reports/MonthlyReportPageTest.cs
+++ test/unit/Quarter.UnitTest/Pages/Application/Reports/MonthlyReportPageTest.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using AngleSharp.Css.Dom;
 using Bunit;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Quarter.Components;
+using Quarter.Core.Models;
 using Quarter.Core.Queries;
 using Quarter.Core.Repositories;
 using Quarter.Core.Utils;
 using Quarter.Pages.Application.Reports;
 using Quarter.Services;
 using Quarter.State;
+using Quarter.State.ViewModels;
 using Quarter.UnitTest.TestUtils;
 using Quarter.Utils;
 using TestContext = Bunit.TestContext;
@@ -101,6 +104,111 @@ public class MonthlyReportPageTest
         }
     }
 
+    [TestFixture]
+    public class WhenTimeIsRegistered : TestCase
+    {
+        private static readonly IdOf<Project> ProjectIdAlpha = IdOf<Project>.Random();
+        private static readonly IdOf<Project> ProjectIdBravo = IdOf<Project>.Random();
+        private static readonly IdOf<Activity> AlphaActivityIdOne = IdOf<Activity>.Random();
+        private static readonly IdOf<Activity> AlphaActivityIdTwo = IdOf<Activity>.Random();
+        private static readonly IdOf<Activity> BravoActivityIdOne = IdOf<Activity>.Random();
+        private static readonly Date FromDate = new Date(DateTime.Parse("2022-03-01T00:00:00Z"));
+        private static readonly Date ToDate = new Date(DateTime.Parse("2022-03-31T00:00:00Z"));
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            //
+            // Setup the projects and activities that are referenced from the result
+            StateManager.State.Projects.Add(new ProjectViewModel
+            {
+                Id = ProjectIdAlpha,
+                Name = "Alpha",
+                Activities = new List<ActivityViewModel>
+                {
+                    new ()
+                    {
+                        Id = AlphaActivityIdOne, Name = "Alpha One", Color = "#aaa", DarkerColor = "#bbb"
+                    },
+                    new ()
+                    {
+                        Id = AlphaActivityIdTwo, Name = "Alpha Two", Color = "#ccc", DarkerColor = "#ddd"
+                    },
+                }
+            });
+            StateManager.State.Projects.Add(new ProjectViewModel
+            {
+                Id = ProjectIdBravo,
+                Name = "Bravo",
+                Activities = new List<ActivityViewModel>
+                {
+                    new ()
+                    {
+                        Id = BravoActivityIdOne, Name = "Bravo One", Color = "#eee", DarkerColor = "#fff"
+                    },
+                }
+            });
+
+            var usage = new Dictionary<Date, IList<ProjectTotalUsage>>
+            {
+                {FromDate, new List<ProjectTotalUsage>
+                    {
+                        new (ProjectIdAlpha,
+                        10 * 15,
+                        new List<ActivityUsage>()
+                        {
+                            new (AlphaActivityIdOne, 6 * 15, UtcDateTime.MinValue),
+                            new (AlphaActivityIdTwo, 4 * 15, UtcDateTime.MinValue),
+                        }, UtcDateTime.MinValue),
+                    }
+                },
+                {ToDate, new List<ProjectTotalUsage>
+                    {
+                        new (ProjectIdAlpha,
+                            10 * 15,
+                            new List<ActivityUsage>()
+                            {
+                                new (AlphaActivityIdOne, 6 * 15, UtcDateTime.MinValue),
+                                new (AlphaActivityIdTwo, 4 * 15, UtcDateTime.MinValue),
+                            }, UtcDateTime.MinValue),
+                        new (ProjectIdBravo, 20 * 15,
+                        new List<ActivityUsage>()
+                        {
+                            new (BravoActivityIdOne, 20 * 15, UtcDateTime.MinValue),
+                        }, UtcDateTime.MinValue)
+                    }
+                },
+            };
+            var result = new UsageOverTime(
+                FromDate,
+                ToDate,
+                120,
+                usage);
+
+            RenderWithResult(result);
+        }
+
+        [Test]
+        public void ItShouldRenderProjectRows()
+            => Assert.That(ProjectRows(), Is.EqualTo(new[]
+            {
+                ("2022-03-01", "Alpha", "2.50"),
+                ("2022-03-31", "Alpha", "2.50"),
+                ("2022-03-31", "Bravo", "5.00"),
+            }));
+
+        [Test]
+        public void ItShouldRenderActivityRows()
+        => Assert.That(ActivityRows(), Is.EqualTo(new[]
+            {
+                ("Alpha One", "rgba(170, 170, 170, 1)", "rgba(187, 187, 187, 1)", "1.50"),
+                ("Alpha Two", "rgba(204, 204, 204, 1)", "rgba(221, 221, 221, 1)", "1.00"),
+                ("Alpha One", "rgba(170, 170, 170, 1)", "rgba(187, 187, 187, 1)", "1.50"),
+                ("Alpha Two", "rgba(204, 204, 204, 1)", "rgba(221, 221, 221, 1)", "1.00"),
+                ("Bravo One", "rgba(238, 238, 238, 1)", "rgba(255, 255, 255, 1)", "5.00"),
+            }));
+    }
+
     public class TestCase : BlazorComponentTestCase<MonthlyReportPage>
     {
         private readonly TestQueryHandler _queryHandler = new ();
@@ -143,9 +251,6 @@ public class MonthlyReportPageTest
         protected EmptyCollectionMessage EmptyMessage()
             => Component?.FindComponent<EmptyCollectionMessage>().Instance;
 
-        protected IEnumerable<string> HeaderColumns()
-            => Component?.FindAll(".qa-report-table thead th").Select(elm => elm.TextContent);
-
         protected void GoToPreviousMonth()
             => Component?.FindAll("[test=action-button]").First().Click();
 
@@ -154,5 +259,29 @@ public class MonthlyReportPageTest
 
         protected string LastNavigatedTo()
             =>_testNavigationManager.LastNavigatedTo();
+
+        protected IEnumerable<(string date, string name, string total)> ProjectRows()
+        {
+            return Component?.FindAll("[test=report-project-row]").Select(elm =>
+            {
+                var date = elm.QuerySelector("[test=row-date]")?.TextContent;
+                var name = elm.QuerySelector("[test=project-name]")?.TextContent;
+                var total = elm.QuerySelector("[test=project-total]")?.TextContent;
+
+                return (date, name, total);
+            });
+        }
+
+        protected IEnumerable<(string name, string bgColor, string borderColor,  string total)> ActivityRows()
+        {
+            return Component?.FindAll("[test=report-activity-row]").Select(elm =>
+            {
+                var name = elm.QuerySelector("[test=activity-name]")?.TextContent;
+                var bgColor = elm.QuerySelector("[test=activity-item-marker]").GetStyle()["background-color"];
+                var borderColor = elm.QuerySelector("[test=activity-item-marker]").GetStyle()["border-color"];
+                var total = elm.QuerySelector("[test=activity-total]")?.TextContent;
+                return (name, bgColor, borderColor, total);
+            });
+        }
     }
 }
