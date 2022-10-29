a17cf55bcb3bdbdb131595dafa5b5a63f32227d3
Markus Eliasson <markus.eliasson@gmail.com>
2022-10-29 09:15:26 +0200

    File scoped namespace

--- test/unit/Quarter.UnitTest/TestUtils/HttpTestSession.cs
+++ test/unit/Quarter.UnitTest/TestUtils/HttpTestSession.cs
@@ -5,32 +5,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 
-namespace Quarter.UnitTest.TestUtils
+namespace Quarter.UnitTest.TestUtils;
+
+public static class HttpTestSession
 {
-    public static class HttpTestSession
+    public static HttpClient HttpClient
+        => _httpClient.Value;
+
+    private static readonly Lazy<HttpClient> _httpClient = CreateClient();
+
+    private static Lazy<HttpClient> CreateClient()
     {
-        public static HttpClient HttpClient
-            => _httpClient.Value;
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.test.json")
+            .AddEnvironmentVariables()
+            .Build();
 
-        private static readonly Lazy<HttpClient> _httpClient = CreateClient();
+        var webBuilder = new WebHostBuilder();
+        webBuilder.UseConfiguration(config);
+        webBuilder.UseStartup(ctx => new Startup(ctx.Configuration));
+        webBuilder.UseEnvironment("Development");
 
-        private static Lazy<HttpClient> CreateClient()
-        {
-            var config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.test.json")
-                .AddEnvironmentVariables()
-                .Build();
+        var startupAssemblyName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+        webBuilder.UseSetting(WebHostDefaults.ApplicationKey, startupAssemblyName);
 
-            var webBuilder = new WebHostBuilder();
-            webBuilder.UseConfiguration(config);
-            webBuilder.UseStartup(ctx => new Startup(ctx.Configuration));
-            webBuilder.UseEnvironment("Development");
-
-            var startupAssemblyName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-            webBuilder.UseSetting(WebHostDefaults.ApplicationKey, startupAssemblyName);
-
-            var server = new TestServer(webBuilder);
-            return new Lazy<HttpClient>(() => server.CreateClient());
-        }
+        var server = new TestServer(webBuilder);
+        return new Lazy<HttpClient>(() => server.CreateClient());
     }
 }
