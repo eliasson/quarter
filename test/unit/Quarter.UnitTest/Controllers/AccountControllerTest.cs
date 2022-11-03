cf92c313ced135557a33772b88aa7824f8050e5d
Markus Eliasson <markus.eliasson@gmail.com>
2022-11-03 12:14:49 +0100

    File scoped namespace

--- test/unit/Quarter.UnitTest/Controllers/AccountControllerTest.cs
+++ test/unit/Quarter.UnitTest/Controllers/AccountControllerTest.cs
@@ -4,65 +4,64 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using Quarter.UnitTest.TestUtils;
 
-namespace Quarter.UnitTest.Controllers
+namespace Quarter.UnitTest.Controllers;
+
+[TestFixture]
+public class AccountControllerTest : HttpTestCase
 {
-    [TestFixture]
-    public class AccountControllerTest : HttpTestCase
+    public class WhenLoggingInUsingGitHub : AccountControllerTest
     {
-        public class WhenLoggingInUsingGitHub : AccountControllerTest
+        private HttpResponseMessage _response;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+            => _response = await LoginAsync("github");
+
+        [Test]
+        public void ItShouldRedirectToGitHubServer()
         {
-            private HttpResponseMessage _response;
-
-            [OneTimeSetUp]
-            public async Task Setup()
-                => _response = await LoginAsync("github");
-
-            [Test]
-            public void ItShouldRedirectToGitHubServer()
+            Assert.Multiple((() =>
             {
-                Assert.Multiple((() =>
-                {
-                    Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.Redirect));
-                    Assert.That(_response.Headers.Location?.ToString(), Does.StartWith("https://github.com/login/oauth/authorize"));
-                }));
-            }
+                Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.Redirect));
+                Assert.That(_response.Headers.Location?.ToString(), Does.StartWith("https://github.com/login/oauth/authorize"));
+            }));
         }
-
-        public class WhenLoggingInUsingGoogle : AccountControllerTest
-        {
-            private HttpResponseMessage _response;
-
-            [OneTimeSetUp]
-            public async Task Setup()
-                => _response = await LoginAsync("google");
-
-            [Test]
-            public void ItShouldRedirectToGoogleServer()
-            {
-                Assert.Multiple((() =>
-                {
-                    Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.Redirect));
-                    Assert.That(_response.Headers.Location?.ToString(), Does.StartWith("https://accounts.google.com/o/oauth2/v2/auth"));
-                }));
-            }
-        }
-
-        public class WhenLoggingInUsingUnknownProvider : AccountControllerTest
-        {
-            private HttpResponseMessage _response;
-
-            [OneTimeSetUp]
-            public async Task Setup()
-                => _response = await LoginAsync("nasa");
-
-            [Test]
-            public void ItShouldResultInNotFoundError()
-            {
-                Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-            }
-        }
-
-        private Task<HttpResponseMessage> LoginAsync(string provider) =>
-            HttpTestSession.HttpClient.GetAsync($"/account/login/{provider}");
     }
+
+    public class WhenLoggingInUsingGoogle : AccountControllerTest
+    {
+        private HttpResponseMessage _response;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+            => _response = await LoginAsync("google");
+
+        [Test]
+        public void ItShouldRedirectToGoogleServer()
+        {
+            Assert.Multiple((() =>
+            {
+                Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.Redirect));
+                Assert.That(_response.Headers.Location?.ToString(), Does.StartWith("https://accounts.google.com/o/oauth2/v2/auth"));
+            }));
+        }
+    }
+
+    public class WhenLoggingInUsingUnknownProvider : AccountControllerTest
+    {
+        private HttpResponseMessage _response;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+            => _response = await LoginAsync("nasa");
+
+        [Test]
+        public void ItShouldResultInNotFoundError()
+        {
+            Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+    }
+
+    private Task<HttpResponseMessage> LoginAsync(string provider) =>
+        HttpTestSession.HttpClient.GetAsync($"/account/login/{provider}");
 }
