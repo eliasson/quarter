9e5c7577919c9032d9acfe6a3a7cce68f9eac5e3
Markus Eliasson <markus.eliasson@gmail.com>
2023-02-18 13:26:11 +0100

    Add resource input for timesheet

--- src/Quarter.HttpApi/Resources/TimesheetResourceInput.cs
+++ src/Quarter.HttpApi/Resources/TimesheetResourceInput.cs
@@ -7,4 +7,23 @@ public class TimesheetResourceInput
     [Required]
     [RegularExpression("^\\d{4}-\\d{2}-\\d{2}$", ErrorMessage = "The date must be given in ISO-8601 (YYYY-MM-DD).")]
     public string? date { get; set; }
+
+    [Required]
+    [MinLength(1, ErrorMessage = "The timeSlots field must not be empty.")]
+    public TimeSlotInput[]? timeSlots { get; set; }
+}
+
+public class TimeSlotInput
+{
+    [Required]
+    public string? projectId { get; set; }
+
+    [Required]
+    public string? activityId { get; set; }
+
+    [Range(0, 95)]
+    public int? offset { get; set; }
+
+    [Range(1, 96)]
+    public int? duration { get; set; } // TODO: This should be validated together with the offset
 }
--- test/unit/Quarter.HttpApi.UnitTest/Resources/TimesheetResourceInputTest.cs
+++ test/unit/Quarter.HttpApi.UnitTest/Resources/TimesheetResourceInputTest.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
+using Quarter.Core.Models;
 using Quarter.Core.Utils;
 using Quarter.HttpApi.Resources;
 
@@ -14,13 +16,19 @@ public class TimesheetResourceInputTest
     {
         public static IEnumerable<object[]> ValidResources()
         {
-            yield return new object[] { new TimesheetResourceInput { date = "2023-01-28" } };
+            yield return new object[] { new TimesheetResourceInput { date = "2023-01-28", timeSlots = new [] { new TimeSlotInput
+            {
+                projectId = IdOf<Project>.Random().AsString(),
+                activityId = IdOf<Activity>.Random().AsString(),
+                offset = 0,
+                duration = 1,
+            } } } };
         }
 
         [TestCaseSource(nameof(ValidResources))]
         public void ItShouldBeValid(TimesheetResourceInput input)
         {
-            var result = ObjectValidator.IsValid(input, out var errors);
+            var result = RecursiveObjectValidator.IsValid(input, out var errors);
             var errorMessages = errors.Select(_ => _.ErrorMessage);
 
             Assert.Multiple(() =>
@@ -36,15 +44,43 @@ public class TimesheetResourceInputTest
     {
         public static IEnumerable<object[]> InvalidResources()
         {
-            yield return new object[] { new TimesheetResourceInput { date = null! }, "The date field is required." };
+            yield return new object[] { new TimesheetResourceInput { date = null }, "The date field is required." };
             yield return new object[] { new TimesheetResourceInput { date = "" }, "The date field is required." };
             yield return new object[] { new TimesheetResourceInput { date = "28-01-23" }, "The date must be given in ISO-8601 (YYYY-MM-DD)." };
+            yield return new object[] { new TimesheetResourceInput { timeSlots = null }, "The timeSlots field is required." };
+            yield return new object[] { new TimesheetResourceInput { timeSlots = Array.Empty<TimeSlotInput>() }, "The timeSlots field must not be empty." };
+            yield return new object[] { new TimesheetResourceInput { date = "2023-01-28", timeSlots = new [] { new TimeSlotInput
+            {
+                activityId = IdOf<Activity>.Random().AsString(),
+                offset = 0,
+                duration = 1,
+            } } }, "The projectId field is required." };
+            yield return new object[] { new TimesheetResourceInput { date = "2023-01-28", timeSlots = new [] { new TimeSlotInput
+            {
+                projectId = IdOf<Project>.Random().AsString(),
+                offset = 0,
+                duration = 1,
+            } } }, "The activityId field is required." };
+            yield return new object[] { new TimesheetResourceInput { date = "2023-01-28", timeSlots = new [] { new TimeSlotInput
+            {
+                projectId = IdOf<Project>.Random().AsString(),
+                activityId = IdOf<Activity>.Random().AsString(),
+                offset = -1,
+                duration = 1,
+            } } }, "The field offset must be between 0 and 95." };
+            yield return new object[] { new TimesheetResourceInput { date = "2023-01-28", timeSlots = new [] { new TimeSlotInput
+            {
+                projectId = IdOf<Project>.Random().AsString(),
+                activityId = IdOf<Activity>.Random().AsString(),
+                offset = 95,
+                duration = 98,
+            } } }, "The field duration must be between 1 and 96." };
         }
 
         [TestCaseSource(nameof(InvalidResources))]
         public void ItShouldNotBeValid(TimesheetResourceInput input, string expectedError)
         {
-            var result = ObjectValidator.IsValid(input, out var errors);
+            var result = RecursiveObjectValidator.IsValid(input, out var errors);
             var errorMessages = errors.Select(_ => _.ErrorMessage);
 
             Assert.Multiple(() =>
