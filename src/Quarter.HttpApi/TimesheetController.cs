d1ec78b8ef7042830bd68b33906a08a8e8d02e1e
Markus Eliasson <markus.eliasson@gmail.com>
2023-04-01 08:55:14 +0200

    Support registering time in timesheet HTTP API

--- src/Quarter.HttpApi/TimesheetController.cs
+++ src/Quarter.HttpApi/TimesheetController.cs
@@ -1,3 +1,4 @@
+using System.Net.Mime;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Quarter.Core.Utils;
@@ -23,5 +24,19 @@ public class TimesheetController : ApiControllerBase
 
         var resource = await ApiService.GetTimesheetAsync(date, oc, ct);
         return Ok(resource);
+    }
+
+    [HttpPut("{isoDate:regex(^\\d{{4}}-\\d{{2}}-\\d{{2}}$)}")]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> RegisterTimeAsync(string isoDate, [FromBody] TimesheetResourceInput input, CancellationToken ct)
+    {
+        var oc = GetOperationContextForCurrentUser();
+        var date = Date.From(isoDate);
+
+        // TODO: Assert that date from path is same as date in input
+        var output = await ApiService.UpdateTimesheetAsync(input, oc, ct);
+
+        return Ok(output);
     }
 }
