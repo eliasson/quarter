2d57278a658304e20271127c5ed2a95b12b3fa9e
Markus Eliasson <markus.eliasson@gmail.com>
2023-10-14 18:11:22 +0200

    Add service for admin functionality

---
+++ src/Quarter/Services/AdminService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+using Quarter.Core.Options;
+
+namespace Quarter.Services;
+
+public interface IAdminService
+{
+    bool IsUserRegistrationOpen();
+}
+
+public class AdminService : IAdminService
+{
+    private readonly IOptions<AuthOptions> _options;
+
+    public AdminService(IOptions<AuthOptions> options)
+    {
+        _options = options;
+    }
+
+    public bool IsUserRegistrationOpen()
+        =>_options.Value.OpenRegistration;
+}
---
+++ test/unit/Quarter.UnitTest/Services/AdminServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using Quarter.Core.Options;
+using Quarter.Services;
+
+namespace Quarter.UnitTest.Services;
+
+[TestFixture]
+public class AdminServiceTest
+{
+    [TestFixture]
+    public class WhenUserRegistrationIsNotConfigured
+    {
+        private AdminService _service;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _service = new AdminService(Options.Create(new AuthOptions()));
+        }
+
+        [Test]
+        public void ItShouldBeDisabled()
+            => Assert.That(_service.IsUserRegistrationOpen(), Is.False);
+    }
+
+    [TestFixture]
+    public class WhenUserRegistrationIsConfiguredToBeOpen
+    {
+        private AdminService _service;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _service = new AdminService(Options.Create(new AuthOptions { OpenRegistration = true }));
+        }
+
+        [Test]
+        public void ItShouldBeEnabled()
+            => Assert.That(_service.IsUserRegistrationOpen(), Is.True);
+    }
+}
