f163bb83b5216917d806e02252ddd467805e26dc
Markus Eliasson <markus.eliasson@gmail.com>
2023-10-15 09:56:17 +0200

    Add method to get system metrics

--- src/Quarter/Services/AdminService.cs
+++ src/Quarter/Services/AdminService.cs
@@ -1,24 +1,44 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using Quarter.Core.Options;
 using Quarter.Core.Repositories;
 
 namespace Quarter.Services;
 
+public record struct SystemMetrics(int NumberOfUsers);
+
 public interface IAdminService
 {
     bool IsUserRegistrationOpen();
+
+    /// <summary>
+    /// Get system metrics that is displayed in the admin view.
+    /// </summary>
+    /// <param name="ct">Token used to cancel the operation</param>
+    /// <returns>System metrics</returns>
+    Task<SystemMetrics> GetSystemMetricsAsync(CancellationToken ct);
 }
 
 public class AdminService : IAdminService
 {
     private readonly IOptions<AuthOptions> _options;
+    private readonly IRepositoryFactory _repositoryFactory;
 
     public AdminService(IOptions<AuthOptions> options,
         IRepositoryFactory repositoryFactory)
     {
         _options = options;
+        _repositoryFactory = repositoryFactory;
     }
 
     public bool IsUserRegistrationOpen()
         => _options?.Value.OpenRegistration ?? false;
+
+    public async Task<SystemMetrics> GetSystemMetricsAsync(CancellationToken ct)
+    {
+        await Task.CompletedTask;
+        var numberOfUsers = await _repositoryFactory.UserRepository().TotalCountAsync(ct);
+        return new SystemMetrics(numberOfUsers);
+    }
 }
--- test/unit/Quarter.UnitTest/Services/AdminServiceTest.cs
+++ test/unit/Quarter.UnitTest/Services/AdminServiceTest.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using NUnit.Framework;
+using Quarter.Core.Models;
 using Quarter.Core.Options;
 using Quarter.Core.Repositories;
+using Quarter.Core.Utils;
 using Quarter.Services;
 
 namespace Quarter.UnitTest.Services;
@@ -33,16 +38,60 @@ public class AdminServiceTest
             => Assert.That(Service.IsUserRegistrationOpen(), Is.True);
     }
 
+    [TestFixture]
+    public class WhenDataExists : TestCase
+    {
+        private SystemMetrics _metrics;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+            SetupTestCase();
+            await AddUsersAsync(12);
+            _metrics = await Service.GetSystemMetricsAsync(CancellationToken.None);
+        }
+
+        [Test]
+        public void ItShouldReturnTotalNumberOfUsersInSystem()
+            => Assert.That(_metrics.NumberOfUsers, Is.EqualTo(12));
+    }
+
     public abstract class TestCase
     {
         protected AdminService Service;
         private readonly InMemoryRepositoryFactory _repositoryFactory = new ();
 
-        protected void SetupTestCase(bool openRegistration)
+        protected void SetupTestCase(bool openRegistration = false)
         {
             Service = new AdminService(
                 Options.Create(new AuthOptions { OpenRegistration = openRegistration }),
                 _repositoryFactory);
+        }
+
+        protected async Task AddUsersAsync(uint numberOfUsers)
+        {
+            for (var i = 0; i < numberOfUsers; i++)
+            {
+                var user = new User(new Email($"{i}@example.com"), ArraySegment<UserRole>.Empty);
+                await _repositoryFactory.UserRepository().CreateAsync(user, CancellationToken.None);
+            }
+        }
+
+        protected async Task AddProjectsAndActivitiesAsync(uint numberOfProjects, uint numberOfActivitiesPerProject)
+        {
+            var user = new User();
+            for (var i = 0; i < numberOfProjects; i++)
+            {
+                var project = new Project("Arbitrary", "");
+                _ = await _repositoryFactory.ProjectRepository(user.Id).CreateAsync(project, CancellationToken.None);
+                for (var j = 0; j < numberOfActivitiesPerProject; j++)
+                {
+                    var activity = new Activity(IdOf<Project>.Random(), "Arbitrary", "",
+                        Color.FromSystemColor(System.Drawing.Color.Violet));
+                    _ = await _repositoryFactory.ActivityRepository(user.Id).CreateAsync(activity, CancellationToken.None);
+                }
+            }
+
         }
     }
 }
