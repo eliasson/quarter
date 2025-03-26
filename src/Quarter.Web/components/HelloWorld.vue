a1c78cda40a180614d476d18f5ba7dfaf92010b5
Markus Eliasson <markus.eliasson@proton.me>
2025-03-26 23:14:15 +0100

    Some initial disposition

--- src/Quarter.Web/components/HelloWorld.vue
+++ src/Quarter.Web/components/HelloWorld.vue
@@ -15,27 +15,3 @@ defineProps<{
   </div>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
