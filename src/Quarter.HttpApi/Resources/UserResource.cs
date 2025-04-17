7b2a4f515bb67e9d7bd3047b23e924cb655556ed
Markus Eliasson <markus.eliasson@gmail.com>
2025-04-18 00:20:12 +0200

    Add user resource

---
+++ src/Quarter.HttpApi/Resources/UserResource.cs
@@ -0,0 +1,22 @@
+﻿using Quarter.Core.Models;
+
+namespace Quarter.HttpApi.Resources;
+
+// ReSharper disable InconsistentNaming
+
+/// <summary>
+/// Output resource for user aggregates
+/// </summary>
+/// <param name="id">The ID of the user</param>
+/// <param name="email">The email address of the user</param>
+/// <param name="created">Timestamp for when the user was created (ISO-8601)</param>
+/// <param name="updated">Timestamp for when the user was last updated, or null if it has never been updated (ISO-8601)</param>
+public record UserResourceOutput(string id, string email, string created, string? updated)
+{
+    public static UserResourceOutput From(User user) =>
+        new(
+            user.Id.ToString(),
+            user.Email.AsString(),
+            user.Created.IsoString(),
+            user.Updated?.IsoString());
+}
---
+++ test/unit/Quarter.HttpApi.UnitTest/Resources/UserResourceOutputTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Quarter.Core.Models;
+using Quarter.Core.Utils;
+using Quarter.HttpApi.Resources;
+
+namespace Quarter.HttpApi.UnitTest.Resources;
+
+[TestFixture]
+public class UserResourceOutputTest
+{
+    [TestFixture]
+    public class WhenConstructingMinimalUserOutput
+    {
+        private User _user = null!;
+        private UserResourceOutput _output = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _user = new User(new Email("jane.doe@example.com"));
+            _output = UserResourceOutput.From(_user);
+        }
+
+        [Test]
+        public void ItShouldMapId()
+            => Assert.That(_output.id, Is.EqualTo(_user.Id.Id.ToString()));
+
+        [Test]
+        public void ItShouldMapEmail()
+            => Assert.That(_output.email, Is.EqualTo(_user.Email.Value));
+
+        [Test]
+        public void ItShouldMapCreatedTimestamp()
+            => Assert.That(_output.created, Is.EqualTo(_user.Created.IsoString()));
+
+        [Test]
+        public void ItShouldLackUpdateTimestamp()
+            => Assert.That(_output.updated, Is.Null);
+    }
+}
+
