294a5ac3d7de50076df52f82ecf071cacb9cedc0
Markus Eliasson <markus.eliasson@proton.me>
2025-05-15 22:10:48 +0200

    Add store type declaration and comment

--- src/Quarter.Web/stores/users.ts
+++ src/Quarter.Web/stores/users.ts
@@ -1,9 +1,18 @@
-﻿import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+﻿import { type Ref, ref} from "vue"
+import { defineStore } from "pinia"
+import type { UserIdentity } from "@/models/user.ts"
 
-export type UserIdentity = { id: string, email: string }
-
-export const useUsersStore = defineStore("users", () => {
+export interface UserState {
+    /** Identities for all the loaded system users. */
+    users: Ref<UserIdentity[]>
+}
+/**
+ * Store to manage the system users.
+ *
+ * While part of the main application, this will only be usable for users with sufficient capabilities and reachable
+ * from the admin views of the application.
+ */
+export const useUsersStore = defineStore("users", (): UserState => {
     const users = ref<UserIdentity[]>([
         { id: "001", email: "jane.doe@example.com" },
         { id: "002", email: "john.doe@example.com" },
