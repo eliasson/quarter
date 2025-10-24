9ddd147890065326a2a3e1ae74f09094d7d0be53
Markus Eliasson <markus.eliasson@proton.me>
2025-10-24 13:28:21 +0200

    Add option for configuring the static path used

--- service/src/Quarter.Core/Options/StorageOptions.cs
+++ service/src/Quarter.Core/Options/StorageOptions.cs
@@ -3,4 +3,5 @@ namespace Quarter.Core.Options;
 public class StorageOptions
 {
     public string DefaultDatabase { get; set; } = string.Empty;
+    public string StaticPath { get; set; } = string.Empty;
 }
