0111255f467d71c54823eccab527a45e968e7575
Markus Eliasson <markus.eliasson@proton.me>
2026-03-18 20:58:21 +0100

    Refactor test and add test for zero activty report

--- service/test/unit/Quarter.HttpApi.UnitTest/Services/ApiService/GetWeeklyReportTest.cs
+++ service/test/unit/Quarter.HttpApi.UnitTest/Services/ApiService/GetWeeklyReportTest.cs
@@ -12,18 +12,9 @@ namespace Quarter.HttpApi.UnitTest.Services;
 public class GetWeeklyReportTest
 {
     [TestFixture]
-    public class WhenNoTimeIsRegistered : TestCase
+    public class WhenTimeIsRegistered : ReportTestCase
     {
         private readonly OperationContext _oc = CreateOperationContext();
-        private static readonly Date Today = Date.Today();
-        private static readonly Date StartOfWeek = Today.StartOfWeek();
-        private static readonly Date EndOfWeek = Today.EndOfWeek();
-
-        private readonly IdOf<Project> _projectIdOne = IdOf<Project>.Random();
-        private readonly IdOf<Project> _projectIdTwo = IdOf<Project>.Random();
-        private readonly IdOf<Activity> _activityIdOne = IdOf<Activity>.Random();
-        private readonly IdOf<Activity> _activityIdTwo = IdOf<Activity>.Random();
-        private readonly IdOf<Activity> _activityIdThree = IdOf<Activity>.Random();
 
         private WeeklyReportResourceOutput _report = null!;
 
@@ -31,12 +22,12 @@ public class GetWeeklyReportTest
         public async Task Setup()
         {
             var startOfWeekTimesheet = Timesheet.CreateForDate(StartOfWeek);
-            startOfWeekTimesheet.Register(new ActivityTimeSlot(_projectIdOne, _activityIdOne, 0, 2));
-            startOfWeekTimesheet.Register(new ActivityTimeSlot(_projectIdTwo, _activityIdTwo, 10, 4));
-            startOfWeekTimesheet.Register(new ActivityTimeSlot(_projectIdTwo, _activityIdTwo, 20, 2));
+            startOfWeekTimesheet.Register(new ActivityTimeSlot(ProjectIdOne, ActivityIdOne, 0, 2));
+            startOfWeekTimesheet.Register(new ActivityTimeSlot(ProjectIdTwo, ActivityIdTwo, 10, 4));
+            startOfWeekTimesheet.Register(new ActivityTimeSlot(ProjectIdTwo, ActivityIdTwo, 20, 2));
 
             var endOfWeekTimesheet = Timesheet.CreateForDate(EndOfWeek);
-            endOfWeekTimesheet.Register(new ActivityTimeSlot(_projectIdOne, _activityIdThree, 30, 8));
+            endOfWeekTimesheet.Register(new ActivityTimeSlot(ProjectIdOne, ActivityIdThree, 30, 8));
 
             await AddTimesheetAsync(_oc.UserId, startOfWeekTimesheet);
             await AddTimesheetAsync(_oc.UserId, endOfWeekTimesheet);
@@ -67,8 +58,8 @@ public class GetWeeklyReportTest
 
             Assert.That(projectsTotal, Is.EquivalentTo(new[]
             {
-                (_projectIdOne.AsString(), 10 * 15),
-                (_projectIdTwo.AsString(), 6 * 15),
+                (ProjectIdOne.AsString(), 10 * 15),
+                (ProjectIdTwo.AsString(), 6 * 15),
             }));
         }
 
@@ -82,8 +73,8 @@ public class GetWeeklyReportTest
 
             Assert.That(result, Is.EquivalentTo(new[]
             {
-                (_activityIdOne.AsString(), 2 * 15),
-                (_activityIdThree.AsString(), 8 * 15),
+                (ActivityIdOne.AsString(), 2 * 15),
+                (ActivityIdThree.AsString(), 8 * 15),
             }));
         }
 
@@ -95,8 +86,57 @@ public class GetWeeklyReportTest
                 .Select(pu => (pu.activityId, pu.weekdayTotals)).FirstOrDefault();
 
             Assert.That(activityUsage, Is.EqualTo(
-                (_activityIdOne.AsString(), new[] { 2 * 15, 0, 0, 0, 0, 0, 0 })
+                (ActivityIdOne.AsString(), new[] { 2 * 15, 0, 0, 0, 0, 0, 0 })
             ));
         }
+    }
+
+    [TestFixture]
+    public class WhenNoTimeIsRegistered : ReportTestCase
+    {
+        private readonly OperationContext _oc = CreateOperationContext();
+
+        private WeeklyReportResourceOutput _report = null!;
+
+        [OneTimeSetUp]
+        public async Task Setup()
+        {
+             _report = await ApiService.GetWeeklyReportAsync(Today, _oc, CancellationToken.None);
+        }
+
+        [Test]
+        public void ItShouldIncludeStartOfWeekAsDateString()
+            => Assert.That(_report.startOfWeek, Is.EqualTo(StartOfWeek.IsoString()));
+
+        [Test]
+        public void ItShouldIncludeEndOfWeekAsDateString()
+            => Assert.That(_report.endOfWeek, Is.EqualTo(EndOfWeek.IsoString()));
+
+        [Test]
+        public void ItShouldIncludeTotalMinutes()
+            => Assert.That(_report.totalMinutes, Is.Zero);
+
+        [Test]
+        public void ItShouldIncludeWeekdayTotals()
+            => Assert.That(_report.weekdayTotals, Is.EqualTo(new[] { 0, 0, 0, 0, 0, 0, 0 }));
+
+        [Test]
+        public void ItShouldIncludeEmptyProjectUsage()
+        {
+            Assert.That(_report.Usage, Is.Empty);
+        }
+    }
+
+    public abstract class ReportTestCase : TestCase
+    {
+        protected static readonly Date Today = Date.Today();
+        protected static readonly Date StartOfWeek = Today.StartOfWeek();
+        protected static readonly Date EndOfWeek = Today.EndOfWeek();
+
+        protected readonly IdOf<Project> ProjectIdOne = IdOf<Project>.Random();
+        protected readonly IdOf<Project> ProjectIdTwo = IdOf<Project>.Random();
+        protected readonly IdOf<Activity> ActivityIdOne = IdOf<Activity>.Random();
+        protected readonly IdOf<Activity> ActivityIdTwo = IdOf<Activity>.Random();
+        protected readonly IdOf<Activity> ActivityIdThree = IdOf<Activity>.Random();
     }
 }
